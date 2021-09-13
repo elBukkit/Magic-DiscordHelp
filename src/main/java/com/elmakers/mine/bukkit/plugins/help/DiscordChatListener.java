0288b8f741b59f32d5fdd697a5d2d74a235e9d39
Nathan Wolf <nathan@elmakers.com>
2021-09-13 15:15:47 -0700

    Add response

--- src/main/java/com/elmakers/mine/bukkit/plugins/help/DiscordChatListener.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/help/DiscordChatListener.java
@@ -1,10 +1,13 @@
 package com.elmakers.mine.bukkit.plugins.help;
 
+import java.util.logging.Level;
+
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.restaction.MessageAction;
 
 public class DiscordChatListener extends ListenerAdapter {
     private final MagicDiscordHelpPlugin controller;
@@ -23,5 +26,7 @@ public class DiscordChatListener extends ListenerAdapter {
 
         String msg = message.getContentDisplay();
         controller.getLogger().info("Got message: " + msg);
+        MessageAction action = channel.sendMessage("Well hello there " + author.getName() + "!");
+        action.queue(sentMessage -> controller.getLogger().info("  Responded!"), throwable -> controller.getLogger().log(Level.SEVERE, "Failed to send message to channel " + channel.getName(), throwable));
     }
 }
