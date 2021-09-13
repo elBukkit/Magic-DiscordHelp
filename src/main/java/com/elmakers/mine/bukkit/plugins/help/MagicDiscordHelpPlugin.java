5124bac3a8c36b27b5661c25b2be1109ee99140d
Nathan Wolf <nathan@elmakers.com>
2021-09-13 12:57:47 -0700

    Add basic plugin framework and maven build

---
+++ src/main/java/com/elmakers/mine/bukkit/plugins/help/MagicDiscordHelpPlugin.java
@@ -0,0 +1,12 @@
+package com.elmakers.mine.bukkit.plugins.help;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MagicDiscordHelpPlugin extends JavaPlugin {
+
+    public void onEnable() {
+    }
+
+    public void onDisable() {
+    }
+}
